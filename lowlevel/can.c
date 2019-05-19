770548c1e4ccdf9301e63221731f69f92dd10db5
Félix Piédallu <felix@piedallu.me>
2019-05-19 16:31:40 +0200

    some fixes, cleanup

--- lowlevel/can.c
+++ lowlevel/can.c
@@ -1,1 +1,19 @@
 #include "can.h"
+
+#include <libopencm3/stm32/gpio.h>
+
+#define GPIO_CAN_PORT   GPIOA
+#define GPIO_CAN_RX_PIN GPIO11
+#define GPIO_CAN_TX_PIN GPIO12
+
+
+void can_setup() {
+	// CAN RX
+	gpio_mode_setup(GPIO_CAN_PORT, GPIO_MODE_INPUT, GPIO_PUPD_PULLUP, GPIO_CAN_RX_PIN);
+	gpio_set_af    (GPIO_CAN_PORT, GPIO_AF9, GPIO_CAN_RX_PIN);
+
+	// CAN TX
+	gpio_mode_setup(GPIO_CAN_PORT, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO_CAN_TX_PIN);
+  gpio_set_af    (GPIO_CAN_PORT, GPIO_AF9, GPIO_CAN_TX_PIN);
+	gpio_set_output_options(GPIO_CAN_PORT, GPIO_OTYPE_PP, GPIO_OSPEED_50MHZ, GPIO_CAN_TX_PIN);
+}
--- lowlevel/gpio.c
+++ lowlevel/gpio.c
@@ -3,23 +3,25 @@
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
 
+#define GPIO_LED_PORT GPIOA
+#define GPIO_LED_PIN  GPIO8
 
 void gpio_setup() {
   rcc_periph_clock_enable(RCC_GPIOA);
 
   // status led
-  gpio_mode_setup(GPIOA, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO8);
+  gpio_mode_setup(GPIO_LED_PORT, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO_LED_PIN);
 }
 
 void led_toggle_status() {
-  gpio_toggle(GPIOA, GPIO8);
+  gpio_toggle(GPIO_LED_PORT, GPIO_LED_PIN);
 }
 
 void led_set_status(uint32_t status) {
   if (status == 0)
-    gpio_clear(GPIOA, GPIO8);
+    gpio_clear(GPIO_LED_PORT, GPIO_LED_PIN);
   else
-    gpio_set  (GPIOA, GPIO8);
+    gpio_set  (GPIO_LED_PORT, GPIO_LED_PIN);
 }
 
 void set_all_1() {
