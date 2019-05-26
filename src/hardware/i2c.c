e1a6b63eea5c88ad66e01ea07cbfcc7846255783
Félix Piédallu <felix@piedallu.me>
2019-05-26 11:04:40 +0200

    small refactoring, no code changes

--- src/hardware/i2c.c
+++ src/hardware/i2c.c
@@ -2,13 +2,54 @@
 
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/i2c.h>
+#include <libopencm3/stm32/gpio.h>
 
 #include "rcc.h"
+
+// I2C
+#define GPIO_I2C_SDA_PORT GPIOB
+#define GPIO_I2C_SDA_PIN  GPIO7
+#define GPIO_I2C_SCL_PORT GPIOA
+#define GPIO_I2C_SCL_PIN  GPIO15
+#define GPIO_I2C_AF       GPIO_AF4
+
+
 
 void i2c_setup(){
   // Enable clocks
   rcc_periph_clock_enable(RCC_I2C1);
+  rcc_periph_clock_enable(RCC_GPIOA);
+  rcc_periph_clock_enable(RCC_GPIOB);
+
   rcc_set_i2c_clock_sysclk(I2C1);
+
+  // I2C
+  gpio_mode_setup(GPIO_I2C_SDA_PORT,
+    GPIO_MODE_AF,
+    GPIO_PUPD_PULLUP,
+    GPIO_I2C_SDA_PIN
+  );
+  gpio_set_af(GPIO_I2C_SDA_PORT, GPIO_I2C_AF, GPIO_I2C_SDA_PIN);
+
+  gpio_mode_setup(GPIO_I2C_SCL_PORT,
+    GPIO_MODE_AF,
+    GPIO_PUPD_PULLUP,
+    GPIO_I2C_SCL_PIN
+  );
+  gpio_set_af(GPIO_I2C_SCL_PORT, GPIO_I2C_AF, GPIO_I2C_SCL_PIN);
+
+  gpio_set_output_options(
+    GPIO_I2C_SDA_PORT,
+    GPIO_OTYPE_OD,
+    GPIO_OSPEED_50MHZ,
+    GPIO_I2C_SDA_PIN
+  );
+  gpio_set_output_options(
+    GPIO_I2C_SCL_PORT,
+    GPIO_OTYPE_OD,
+    GPIO_OSPEED_50MHZ,
+    GPIO_I2C_SCL_PIN
+  );
 
   // Reset
   i2c_reset(I2C1);
