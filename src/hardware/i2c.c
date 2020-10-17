0d7a317f5ed09653422841acbe129e436f64ec3d
Antonin Hirschy <antonin.hirschy@grenoble-inp.org>
2020-10-17 19:55:50 +0200

    Update & fixes for I2C

--- src/hardware/i2c.c
+++ src/hardware/i2c.c
@@ -1,128 +1,255 @@
 #include "i2c.h"
+#include "utils.h"
+#include "task.h"
 
-#include "rcc.h"
-#include "stm32f3xx_hal.h"
-
-#define I2C1_SCL_ToF_Pin GPIO_PIN_15
-#define I2C1_SCL_ToF_GPIO_Port GPIOA
-#define I2C1_SDA_ToF_Pin GPIO_PIN_7
-#define I2C1_SDA_ToF_GPIO_Port GPIOB
-
-GPIO_InitTypeDef GPIO_InitStruct;
-I2C_HandleTypeDef hi2c1;
-int i2c_setup() {
-  __HAL_RCC_GPIOA_CLK_ENABLE();
-  __HAL_RCC_GPIOB_CLK_ENABLE();
-  /**I2C1 GPIO Configuration    
-     PA15     ------> I2C1_SCL
-     PB7     ------> I2C1_SDA 
-  */
-  GPIO_InitStruct.Pin = I2C1_SCL_ToF_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_AF_OD;
-  GPIO_InitStruct.Pull = GPIO_PULLUP;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
-  GPIO_InitStruct.Alternate = GPIO_AF4_I2C1;
-  HAL_GPIO_Init(I2C1_SCL_ToF_GPIO_Port, &GPIO_InitStruct);
-
-  GPIO_InitStruct.Pin = I2C1_SDA_ToF_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_AF_OD;
-  GPIO_InitStruct.Pull = GPIO_PULLUP;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
-  GPIO_InitStruct.Alternate = GPIO_AF4_I2C1;
-  HAL_GPIO_Init(I2C1_SDA_ToF_GPIO_Port, &GPIO_InitStruct);
-
-  /* I2C1 clock enable */
-  __HAL_RCC_I2C1_CLK_ENABLE();
-  
-  hi2c1.Instance = I2C1;
-  hi2c1.Init.Timing = 0x0000020B;
-  hi2c1.Init.OwnAddress1 = 0;
-  hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
-  hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
-  hi2c1.Init.OwnAddress2 = 0;
-  hi2c1.Init.OwnAddress2Masks = I2C_OA2_NOMASK;
-  hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
-  hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
-  if (HAL_I2C_Init(&hi2c1) != HAL_OK)
-  {
-    return -1;
-  }
-  /** Configure Analogue filter 
-  */
-  if (HAL_I2CEx_ConfigAnalogFilter(&hi2c1, I2C_ANALOGFILTER_ENABLE) != HAL_OK)
-  {
-    return -1;
-  }
-  /** Configure Digital filter 
-  */
-  if (HAL_I2CEx_ConfigDigitalFilter(&hi2c1, 0) != HAL_OK)
-  {
-    return -1;
-  }
-
-  return 0;
+/**********************/
+/* I2C initialization */
+/**********************/
+void i2c_setup()
+{
+    // I2C1
+    i2c_peripheral_disable(I2C1);
+    i2c_enable_analog_filter(I2C1); // filters up to 50 ns pulses
+    i2c_set_speed(I2C1, i2c_speed_fm_400k, I2C1_CLK_MHZ);
+    
+    // set_speed fix : 125ns @8MHz, see RM0316 §28.4.9 Table 147
+    i2c_set_data_hold_time(I2C1, 1);
+    
+    i2c_set_7bit_addr_mode(I2C1);
+    i2c_enable_stretching(I2C1); // required for master mode (default)
+    
+    // Enable transceiver and peripheral
+    gpio_set(GPIO_I2CEN_PORT, GPIO_I2CEN_PIN);
+    i2c_peripheral_enable(I2C1);
 }
 
-#if 0
-#include <libopencm3/stm32/rcc.h>
-#include <libopencm3/stm32/i2c.h>
-#include <libopencm3/stm32/gpio.h>
+/*********************/
+/* I2C communication */
+/*********************/
 
-#define GPIO_I2C_SDA_PORT GPIOB
-#define GPIO_I2C_SDA_PIN  GPIO7
-#define GPIO_I2C_SCL_PORT GPIOA
-#define GPIO_I2C_SCL_PIN  GPIO15
-#define GPIO_I2C_AF       GPIO_AF4
+#define i2c_enable_reload(i2c) SET_REG(I2C_CR2(i2c), I2C_CR2_RELOAD)
+#define i2c_disable_reload(i2c) CLEAR_REG(I2C_CR2(i2c), I2C_CR2_RELOAD)
 
-void i2c_setup_libopencm3(){
-  // Enable clocks
-  rcc_periph_clock_enable(RCC_I2C1);
-  rcc_periph_clock_enable(RCC_GPIOA);
-  rcc_periph_clock_enable(RCC_GPIOB);
+enum I2C_Dir{
+    I2C_READ = 0, I2C_WRITE = 1
+};
 
-  rcc_set_i2c_clock_sysclk(I2C1);
+/**
+ * @brief Wait until TXIS or NACKF is set
+ * 
+ * @param i2c the peripheral address
+ * @param start start tick for timeout reference
+ * @param timeout maximum amount of time to wait
+ * @return I2C_Status status code
+ */
+static I2C_Status i2c_wait_tx(uint32_t i2c, tick_t start, tick_t timeout);
 
-  // I2C
-  gpio_mode_setup(GPIO_I2C_SDA_PORT,
-    GPIO_MODE_AF,
-    GPIO_PUPD_PULLUP,
-    GPIO_I2C_SDA_PIN
-  );
-  gpio_set_af(GPIO_I2C_SDA_PORT, GPIO_I2C_AF, GPIO_I2C_SDA_PIN);
+/**
+ * @brief Configure the I2C NBYTES value and the RELOAD bit, given the total
+ * number of bytes left to transfer
+ *
+ * @param i2c the i2c peripheral address
+ * @param n total number of bytes to transfer
+ * @return uint8_t the NBYTES value which has been set
+ */
+static uint8_t i2c_set_nbytes(uint32_t i2c, size_t n);
 
-  gpio_mode_setup(GPIO_I2C_SCL_PORT,
-    GPIO_MODE_AF,
-    GPIO_PUPD_PULLUP,
-    GPIO_I2C_SCL_PIN
-  );
-  gpio_set_af(GPIO_I2C_SCL_PORT, GPIO_I2C_AF, GPIO_I2C_SCL_PIN);
+/**
+ * @brief Initializes a master transfer
+ * 
+ * @param i2c peripheral address
+ * @param addr slave address
+ * @param n bytes to transfer
+ * @param dir transfer direction
+ * @param stop enable or disable stop generation after transfer
+ * @return uint8_t nbytes value
+ */
+static uint8_t i2c_master_start7(uint32_t i2c, uint8_t addr, size_t n, enum I2C_Dir dir, bool stop);
 
-  gpio_set_output_options(
-    GPIO_I2C_SDA_PORT,
-    GPIO_OTYPE_OD,
-    GPIO_OSPEED_50MHZ,
-    GPIO_I2C_SDA_PIN
-  );
-  gpio_set_output_options(
-    GPIO_I2C_SCL_PORT,
-    GPIO_OTYPE_OD,
-    GPIO_OSPEED_50MHZ,
-    GPIO_I2C_SCL_PIN
-  );
+/**
+ * @brief Wait for an I2C transfer to end
+ * 
+ * @param i2c peripheral address
+ * @param stop true if stop generation is enabled, false otherwise
+ * @param t_start timeout start reference
+ * @param timeout timeout
+ * @return I2C_Status status code
+ */
+static I2C_Status i2c_wait_end(uint32_t i2c, bool stop, tick_t t_start, tick_t timeout);
 
-  // Reset
-  i2c_reset(I2C1);
-  i2c_peripheral_disable(I2C1);
-
-  // Configuration
-  i2c_enable_analog_filter(I2C1);
-  i2c_set_digital_filter(I2C1, 0); // no filter
-  i2c_set_speed(I2C1, i2c_speed_fm_400k, 64); // 400kHz mode
-  i2c_enable_stretching(I2C1);
-  i2c_set_7bit_addr_mode(I2C1);
-
-  // Enable I2C
-  i2c_peripheral_enable(I2C1);
+I2C_Status i2c_master_send7(uint32_t i2c, uint8_t addr,
+                            uint8_t *wbuff, size_t wn,
+                            tick_t timeout, bool stop)
+{
+    if(!wn) return I2C_OK;
+    tick_t t_start = get_systick();
+    
+    // Init transfer
+    uint8_t nbytes = i2c_master_start7(i2c, addr, wn, I2C_WRITE, stop);
+    /*
+     * What can go wrong now :
+     * -> Arbitration lost
+     * -> NACKF
+     * -> Timeout (busy bus, may require reset)
+     */
+    do{
+        // Send nbytes data
+        for(int i = 0; i < nbytes; ++i){
+            // Wait for TX buffer
+            I2C_Status ret = i2c_wait_tx(i2c, t_start, timeout);
+            if(ret != I2C_OK)
+                return ret;
+            
+            // Write data
+            i2c_send_data(i2c, wbuff[i]);
+        }
+        
+        wbuff += nbytes;
+        wn -= nbytes;
+        if(wn){
+            // Reload mode was set, wait for TCR flag
+            if(!wait_flag(&I2C_ISR(i2c), I2C_ISR_TCR, SET, t_start, timeout))
+                return I2C_ETIME;
+            // Configure next transfer
+            nbytes = i2c_set_nbytes(i2c, wn);
+        }
+    }while(wn);
+    
+    return i2c_wait_end(i2c, stop, t_start, timeout);
 }
-#endif
 
+I2C_Status i2c_master_recv7(uint32_t i2c, uint8_t addr,
+                               uint8_t *rbuff, size_t rn,
+                               TickType_t timeout, bool stop)
+{
+    if(!rn) return I2C_OK;
+    tick_t t_start = get_systick();
+    
+    // Init transfer
+    uint8_t nbytes = i2c_master_start7(i2c, addr, rn, I2C_READ, stop);
+
+    do{
+        // Read nbytes of data
+        for(int i = 0; i < nbytes; ++i){
+            // Wait for RX buffer
+            if(!wait_flag(&I2C_ISR(i2c), I2C_ISR_RXNE, SET, t_start, timeout)){
+                return I2C_ETIME;
+            }
+            // Read data
+            rbuff[i] = i2c_get_data(i2c);
+        }
+        
+        rbuff += nbytes;
+        rn -= nbytes;
+        if(rn){
+            // Reload mode was set, wait for TCR flag
+            if(!wait_flag(&I2C_ISR(i2c), I2C_ISR_TCR, SET, t_start, timeout))
+                return I2C_ETIME;
+            // Configure next transfer
+            nbytes = i2c_set_nbytes(i2c, rn);
+        }
+    }while(rn);
+    
+    return i2c_wait_end(i2c, stop, t_start, timeout);
+}
+
+I2C_Status i2c_master_transfer7(uint32_t i2c, uint8_t addr,
+                                uint8_t *wbuff, size_t wn,
+                                uint8_t *rbuff, size_t rn,
+                                tick_t timeout)
+{
+    I2C_Status ret;
+    ret = i2c_master_send7(i2c, addr, wbuff, wn, timeout, rn == 0);
+    if(ret != I2C_OK)
+        return ret;
+    return i2c_master_recv7(i2c, addr, rbuff, rn, timeout, true);
+}
+
+static I2C_Status i2c_wait_tx(uint32_t i2c, tick_t start, tick_t timeout)
+{
+    while(!i2c_transmit_int_status(i2c)){
+        if(i2c_nack(i2c)){
+            // Don't wait anymore, TXIS won't be set
+            I2C_ICR(i2c) = I2C_ICR_NACKCF;
+            return I2C_ENACK;
+        }else if(timeout == MAX_DELAY){
+            // And I think it's gonna be a long long time
+            continue;
+        }else if((timeout == 0)
+                 || ((get_systick() - start) > timeout)){
+            return I2C_ETIME;
+        }
+    }
+    
+    return I2C_OK;
+}
+
+static uint8_t i2c_set_nbytes(uint32_t i2c, size_t n)
+{
+    uint8_t nbytes;
+    if(n > I2C_MAX_NBYTES){
+        nbytes = I2C_MAX_NBYTES;
+        i2c_enable_reload(i2c);
+    }else{
+        nbytes = n;
+        i2c_disable_reload(i2c);
+    }
+    i2c_set_bytes_to_transfer(i2c, n);
+    
+    return nbytes;
+}
+
+static uint8_t i2c_master_start7(uint32_t i2c, uint8_t addr, size_t n, enum I2C_Dir dir, bool stop)
+{
+    // 7-bit address mode is expected to be set
+    i2c_set_7bit_address(i2c, addr);
+    
+    // Direction
+    if(dir == I2C_READ){
+        i2c_set_read_transfer_dir(i2c);
+    }else{
+        i2c_set_write_transfer_dir(i2c);
+    }
+    
+    uint8_t nbytes = i2c_set_nbytes(i2c, n);
+    
+    // NACK is supposed to be cleared
+    i2c_send_start(i2c);
+    /*
+     * Checking for a busy bus condition before START is useless because the
+     * BUSY flag could be set just after the read.
+     */
+     
+    // Set autoend (for the last transfer)
+    if(stop){
+        i2c_enable_autoend(i2c);
+    }else{
+        i2c_disable_autoend(i2c);
+    }
+    /*
+     * AUTOEND mode is set after start to avoid sending a stop if the user wants
+     * a restart
+     */
+    
+    
+    return nbytes;
+}
+
+static I2C_Status i2c_wait_end(uint32_t i2c, bool stop, tick_t t_start, tick_t timeout)
+{
+    if(stop){
+        // Wait stop flag
+        if(!wait_flag(&I2C_ISR(i2c), I2C_ISR_STOPF, SET, t_start, timeout)){
+            return I2C_ETIME;
+        }
+        I2C_ICR(i2c) = I2C_ICR_STOPCF;
+    }else{
+        // Wait transfer complete flag
+        if(!wait_flag(&I2C_ISR(i2c), I2C_ISR_TC, SET, t_start, timeout)){
+            return I2C_ETIME;
+        }
+        // The flag will be cleared with a future STOP or START
+    }
+    
+    return I2C_OK;
+}
--- src/hardware/i2c.h
+++ src/hardware/i2c.h
@@ -1,6 +1,71 @@
 #ifndef I2C_H
 #define I2C_H
 
-int i2c_setup();
+#include <libopencm3/stm32/i2c.h>
+#include "FreeRTOS.h"
+#include "utils.h"
 
-#endif
+#define I2C1_CLK_MHZ    8
+#define I2C_MAX_NBYTES 255
+
+typedef enum I2C_Status_E{
+    I2C_OK,
+    I2C_ETIME, // timeout for unknown reason
+    I2C_ENACK, // NACK
+}I2C_Status;
+
+/**
+ * @brief Set the application-specific I2C configuration
+ * @warning GPIOs must be initialized
+ */
+void i2c_setup();
+
+/**
+ * @brief Send data to an I2C device
+ * 
+ * @param i2c the I2C peripheral address
+ * @param addr address of the destination device
+ * @param wbuff data buffer to write
+ * @param wn number of bytes to write
+ * @param timeout maximum amount of ticks to wait
+ * @param stop true to send STOP at the end, false otherwise
+ * @return I2C_Status return status code
+ */
+I2C_Status i2c_master_send7(uint32_t i2c, uint8_t addr,
+                             uint8_t *wbuff, size_t wn,
+                             tick_t timeout, bool stop);
+
+/**
+ * @brief Read data from an I2C device
+ * 
+ * @param i2c the I2C peripheral address
+ * @param addr address of the slave
+ * @param rbuff data buffer
+ * @param rn number of bytes to read
+ * @param timeout maximum amount of ticks to wait
+ * @param stop true to send STOP at the end, false otherwise
+ * @return I2C_Status return status code
+ */
+I2C_Status i2c_master_recv7(uint32_t i2c, uint8_t addr,
+                               uint8_t *rbuff, size_t rn,
+                               tick_t timeout, bool stop);
+
+/**
+ * @brief Perform an I2C write followed by a read.
+ * This function is useful for I2C memory accesses.
+ * 
+ * @param i2c the I2C peripheral address
+ * @param addr the slave address
+ * @param wbuff data buffer to write
+ * @param wn number of bytes to write
+ * @param rbuff data buffer for read
+ * @param rn number of bytes to read
+ * @param timeout maximum amount of ticks to wait
+ * @return I2C_Status return status code
+ */
+I2C_Status i2c_master_transfer7(uint32_t i2c, uint8_t addr,
+                                uint8_t *wbuff, size_t wn,
+                                uint8_t *rbuff, size_t rn,
+                                tick_t timeout);
+
+#endif /* I2C_H */
