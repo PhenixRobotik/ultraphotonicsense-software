825c8f18948cff5e44b8def7f312d8d182975125
Antonin Hirschy <antonin.hirschy@grenoble-inp.org>
2020-10-17 19:46:57 +0200

    Update GPIO setup

--- src/hardware/gpio.c
+++ src/hardware/gpio.c
@@ -1,54 +1,76 @@
 #include "gpio.h"
 
 void gpio_setup() {
-  rcc_periph_clock_enable(RCC_GPIOA);
-  rcc_periph_clock_enable(RCC_GPIOB);
 
-  // Satus led
-  gpio_mode_setup(GPIO_LED_PORT, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO_LED_PIN);
+  /* Status LED */
+  gpio_set_output_options(GPIO_LED_PORT, GPIO_OTYPE_PP,
+                          GPIO_OSPEED_2MHZ, GPIO_LED_PIN);
+  gpio_mode_setup(GPIO_LED_PORT, GPIO_MODE_OUTPUT,
+                  GPIO_PUPD_NONE, GPIO_LED_PIN);
 
+  /* bxCAN */
   // CAN RX
-  gpio_mode_setup(GPIO_CAN_PORT, GPIO_MODE_AF, GPIO_PUPD_PULLUP, GPIO_CAN_RX_PIN);
-  gpio_set_af(GPIO_CAN_PORT, GPIO_CAN_AF, GPIO11);
-
+  gpio_set_af(GPIO_CAN_PORT, GPIO_CAN_AF, GPIO_CAN_RX_PIN);
+  gpio_mode_setup(GPIO_CAN_PORT, GPIO_MODE_AF,
+                  GPIO_PUPD_PULLUP, GPIO_CAN_RX_PIN);
+  
   // CAN TX
-  gpio_mode_setup(GPIO_CAN_PORT, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO_CAN_TX_PIN);
-  gpio_set_output_options(GPIO_CAN_PORT, GPIO_OTYPE_PP, GPIO_OSPEED_50MHZ, GPIO_CAN_TX_PIN);
   gpio_set_af(GPIO_CAN_PORT, GPIO_CAN_AF, GPIO_CAN_TX_PIN);
+  gpio_set_output_options(GPIO_CAN_PORT, GPIO_OTYPE_PP,
+                          GPIO_OSPEED_50MHZ, GPIO_CAN_TX_PIN);
+  gpio_mode_setup(GPIO_CAN_PORT, GPIO_MODE_AF,
+                  GPIO_PUPD_NONE, GPIO_CAN_TX_PIN);  
   
-  // Shift register
-  gpio_mode_setup(GPIO_TOF_RESET_PORT, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE,
-		  GPIO_TOF_RESET_DATA_PIN | GPIO_TOF_RESET_SHIFT_PIN);
+  /* I2C */
+  gpio_set_af(GPIO_I2C1_SCL_PORT, GPIO_I2C_AF, GPIO_I2C1_SCL_PIN);
+  gpio_set_af(GPIO_I2C1_SDA_PORT, GPIO_I2C_AF, GPIO_I2C1_SDA_PIN);
+	gpio_set_output_options(GPIO_I2C1_SCL_PORT, GPIO_OTYPE_OD,
+                          GPIO_OSPEED_100MHZ,	GPIO_I2C1_SCL_PIN);
+  gpio_set_output_options(GPIO_I2C1_SDA_PORT, GPIO_OTYPE_OD,
+                          GPIO_OSPEED_100MHZ,	GPIO_I2C1_SDA_PIN);
+	gpio_mode_setup(GPIO_I2C1_SCL_PORT, GPIO_MODE_AF,
+                  GPIO_PUPD_PULLUP, GPIO_I2C1_SCL_PIN);
+  gpio_mode_setup(GPIO_I2C1_SDA_PORT, GPIO_MODE_AF,
+                  GPIO_PUPD_PULLUP, GPIO_I2C1_SDA_PIN);
+  
+  // I2C enable
+  gpio_set_output_options(GPIO_I2CEN_PORT, GPIO_OTYPE_PP,
+                          GPIO_OSPEED_2MHZ, GPIO_I2CEN_PIN);
+  gpio_mode_setup(GPIO_I2CEN_PORT, GPIO_MODE_OUTPUT,
+                  GPIO_PUPD_PULLUP, GPIO_I2CEN_PIN);
+  
+  /* Shift register */
+  gpio_set_output_options(GPIO_TOF_RST_PORT, GPIO_OTYPE_PP, GPIO_OSPEED_2MHZ,
+                          GPIO_TOF_RST_DATA_PIN | GPIO_TOF_RST_SHIFT_PIN);
+  gpio_mode_setup(GPIO_TOF_RST_PORT, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE,
+                  GPIO_TOF_RST_DATA_PIN | GPIO_TOF_RST_SHIFT_PIN);
 
-  // I2C : managed by ST-HAL
-#if 0
-  gpio_mode_setup(GPIO_I2C_SDA_PORT, GPIO_MODE_AF, GPIO_PUPD_PULLUP, GPIO_I2C_SDA_PIN);
-  gpio_set_af(GPIO_I2C_SDA_PORT, GPIO_I2C_AF, GPIO_I2C_SDA_PIN);
+  /* UART1 */
+  gpio_set_af(GPIO_UART1_PORT, GPIO_UART1_AF,
+              GPIO_UART1_RX_PIN | GPIO_UART1_TX_PIN);
+  gpio_set_output_options(GPIO_UART1_PORT, GPIO_OTYPE_PP,
+                          GPIO_OSPEED_100MHZ, GPIO_UART1_TX_PIN);
+  gpio_mode_setup(GPIO_UART1_PORT, GPIO_MODE_AF, GPIO_PUPD_PULLUP,
+                  GPIO_UART1_RX_PIN | GPIO_UART1_TX_PIN);
   
-  gpio_mode_setup(GPIO_I2C_SCL_PORT, GPIO_MODE_AF, GPIO_PUPD_PULLUP, GPIO_I2C_SCL_PIN);
-  gpio_set_af(GPIO_I2C_SCL_PORT, GPIO_I2C_AF, GPIO_I2C_SCL_PIN);
-
-  gpio_set_output_options(GPIO_I2C_SDA_PORT, GPIO_OTYPE_OD, GPIO_OSPEED_50MHZ, GPIO_I2C_SDA_PIN);
-  gpio_set_output_options(GPIO_I2C_SCL_PORT, GPIO_OTYPE_OD, GPIO_OSPEED_50MHZ, GPIO_I2C_SCL_PIN);
-#endif
+  /* Ultrasonic sensors */
+  // Output enable
+  gpio_set_output_options(GPIO_US_OE_PORT, GPIO_OTYPE_PP,
+                          GPIO_OSPEED_2MHZ, GPIO_US_OE_PIN);
+  gpio_mode_setup(GPIO_US_OE_PORT, GPIO_MODE_OUTPUT,
+                  GPIO_PUPD_NONE, GPIO_US_OE_PIN);
+  // Trigger pins
+  gpio_set_output_options(GPIO_US_PORT, GPIO_OTYPE_PP,
+                          GPIO_OSPEED_2MHZ, GPIO_US_TRIG_PINS);
+  gpio_mode_setup(GPIO_US_PORT, GPIO_MODE_OUTPUT,
+                  GPIO_PUPD_NONE, GPIO_US_TRIG_PINS);
+  // Echo pins
+  gpio_mode_setup(GPIO_US_PORT, GPIO_MODE_INPUT,
+                  GPIO_PUPD_PULLDOWN, GPIO_US_ECHO_PINS);
   
-  // UART
-  gpio_mode_setup(GPIO_UART1_PORT, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO_UART1_RX_PIN);
-  gpio_set_af(GPIO_UART1_PORT, GPIO_UART1_AF, GPIO_UART1_RX_PIN);
-  
-  gpio_mode_setup(GPIO_UART1_PORT, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO_UART1_TX_PIN);
-  gpio_set_af(GPIO_UART1_PORT, GPIO_UART1_AF, GPIO_UART1_TX_PIN);
-
-  // Ultrasonic Sensors
-  gpio_mode_setup(GPIO_US_OE_PORT, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO_US_OE_PIN);
-  gpio_mode_setup(GPIO_US_TRIG_0_PORT, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO_US_TRIG_0_PIN);
-  gpio_mode_setup(GPIO_US_ECHO_0_PORT, GPIO_MODE_INPUT, GPIO_PUPD_NONE, GPIO_US_ECHO_0_PIN);
-  gpio_mode_setup(GPIO_US_TRIG_1_PORT, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO_US_TRIG_1_PIN);
-  gpio_mode_setup(GPIO_US_ECHO_1_PORT, GPIO_MODE_INPUT, GPIO_PUPD_NONE, GPIO_US_ECHO_1_PIN);
-  gpio_mode_setup(GPIO_US_TRIG_2_PORT, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO_US_TRIG_2_PIN);
-  gpio_mode_setup(GPIO_US_ECHO_2_PORT, GPIO_MODE_INPUT, GPIO_PUPD_NONE, GPIO_US_ECHO_2_PIN);
-  gpio_mode_setup(GPIO_US_TRIG_3_PORT, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO_US_TRIG_3_PIN);
-  gpio_mode_setup(GPIO_US_ECHO_3_PORT, GPIO_MODE_INPUT, GPIO_PUPD_NONE, GPIO_US_ECHO_3_PIN);  
+  // Lock configuration
+  gpio_port_config_lock(GPIOA, 0xFFFF);
+  gpio_port_config_lock(GPIOB, 0xFFFF);
 }
 
 void led_on() {
