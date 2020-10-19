b699e39d7cc0083c03511cfd36f99fd93e633889
Antonin Hirschy <antonin.hirschy@grenoble-inp.org>
2020-10-19 02:57:59 +0200

    VL53L3CX platform : removed unused stuff

--- src/vl53l3cx/vl53lx_platform.c
+++ src/vl53l3cx/vl53lx_platform.c
@@ -44,218 +44,121 @@
 #include "vl53lx_platform.h"
 #include <vl53lx_platform_log.h>
 
-#define VL53LX_get_register_name(VL53LX_p_007,VL53LX_p_032) VL53LX_COPYSTRING(VL53LX_p_032, "");
+#define VL53LX_get_register_name(VL53LX_p_007, VL53LX_p_032)                   \
+	VL53LX_COPYSTRING(VL53LX_p_032, "");
 
 const uint32_t _power_board_in_use = 0;
 
-
 uint32_t _power_board_extended = 0;
-
 
 uint8_t global_comms_type = 0;
 
-#define  VL53LX_COMMS_BUFFER_SIZE 64
+#define VL53LX_COMMS_BUFFER_SIZE 64
 
-#define trace_print(level, ...) \
-	_LOG_TRACE_PRINT(VL53LX_TRACE_MODULE_PLATFORM, \
-	level, VL53LX_TRACE_FUNCTION_NONE, ##__VA_ARGS__)
+#define trace_print(level, ...)                                                \
+	_LOG_TRACE_PRINT(VL53LX_TRACE_MODULE_PLATFORM, level,                  \
+			 VL53LX_TRACE_FUNCTION_NONE, ##__VA_ARGS__)
 
-#define trace_i2c(...) \
-	_LOG_TRACE_PRINT(VL53LX_TRACE_MODULE_NONE, \
-	VL53LX_TRACE_LEVEL_NONE, VL53LX_TRACE_FUNCTION_I2C, ##__VA_ARGS__)
+#define trace_i2c(...)                                                         \
+	_LOG_TRACE_PRINT(VL53LX_TRACE_MODULE_NONE, VL53LX_TRACE_LEVEL_NONE,    \
+			 VL53LX_TRACE_FUNCTION_I2C, ##__VA_ARGS__)
 
-
-VL53LX_Error VL53LX_CommsInitialise(
-	VL53LX_Dev_t *pdev,
-	uint8_t       comms_type,
-	uint16_t      comms_speed_khz)
+VL53LX_Error VL53LX_WriteMulti(VL53LX_Dev_t *pdev, uint16_t index,
+			       uint8_t *pdata, uint32_t count)
 {
 	VL53LX_Error status = VL53LX_ERROR_NONE;
-
-	SUPPRESS_UNUSED_WARNING(pdev);
-	SUPPRESS_UNUSED_WARNING(comms_speed_khz);
-
-	global_comms_type = comms_type;
-
-	if(global_comms_type == VL53LX_I2C)
-	{
-		// TODO
-		//status = VL53LX_ERROR_CONTROL_INTERFACE;
-	}
-	else if(global_comms_type == VL53LX_SPI)
-	{
-		// unused
-	}
-	else
-	{
-		trace_i2c("VL53LX_CommsInitialise: Comms must be one of VL53LX_I2C or VL53LX_SPI\n");
-		status = VL53LX_ERROR_CONTROL_INTERFACE;
-	}
-
-	return status;
-}
-
-
-VL53LX_Error VL53LX_CommsClose(
-	VL53LX_Dev_t *pdev)
-{
-	VL53LX_Error status = VL53LX_ERROR_NONE;
-
-	SUPPRESS_UNUSED_WARNING(pdev);
-
-	if(global_comms_type == VL53LX_I2C)
-	{
-		// TODO
-		//use VL53LX_ERROR_CONTROL_INTERFACE status on error
-	}
-	else if(global_comms_type == VL53LX_SPI)
-	{
-		// unused
-	}
-	else
-	{
-		trace_i2c("VL53LX_CommsClose: Comms must be one of VL53LX_I2C or VL53LX_SPI\n");
-		status = VL53LX_ERROR_CONTROL_INTERFACE;
-	}
-
-	return status;
-}
-
-
-
-VL53LX_Error VL53LX_WriteMulti(
-	VL53LX_Dev_t *pdev,
-	uint16_t      index,
-	uint8_t      *pdata,
-	uint32_t      count)
-{
-	VL53LX_Error status         = VL53LX_ERROR_NONE;
-	uint32_t     position       = 0;
 
 	_LOG_STRING_BUFFER(register_name);
 	_LOG_STRING_BUFFER(value_as_str);
 
-	if(global_comms_type == VL53LX_I2C)
-	{
+	if (global_comms_type == VL53LX_I2C) {
 		// TODO
 
-		if(status != VL53LX_ERROR_NONE)
-		{
-			trace_i2c("VL53LX_WriteMulti RANGING_SENSOR_COMMS_Write_CCI() failed\n");
+		if (status != VL53LX_ERROR_NONE) {
+			trace_i2c(
+				"VL53LX_WriteMulti RANGING_SENSOR_COMMS_Write_CCI() failed\n");
 		}
-	}
-	else if(global_comms_type == VL53LX_SPI)
-	{
+	} else if (global_comms_type == VL53LX_SPI) {
 		// unused
-	}
-	else
-	{
-		trace_i2c("VL53LX_WriteMulti: Comms must be one of VL53LX_I2C or VL53LX_SPI\n");
+	} else {
+		trace_i2c(
+			"VL53LX_WriteMulti: Comms must be one of VL53LX_I2C or VL53LX_SPI\n");
 		status = VL53LX_ERROR_CONTROL_INTERFACE;
 	}
 
 	return status;
 }
 
-
-VL53LX_Error VL53LX_ReadMulti(
-	VL53LX_Dev_t *pdev,
-	uint16_t      index,
-	uint8_t      *pdata,
-	uint32_t      count)
+VL53LX_Error VL53LX_ReadMulti(VL53LX_Dev_t *pdev, uint16_t index,
+			      uint8_t *pdata, uint32_t count)
 {
-	VL53LX_Error status         = VL53LX_ERROR_NONE;
-	uint32_t     position       = 0;
-	uint32_t     data_size      = 0;
+	VL53LX_Error status = VL53LX_ERROR_NONE;
 
 	_LOG_STRING_BUFFER(register_name);
 	_LOG_STRING_BUFFER(value_as_str);
 
-	if(global_comms_type == VL53LX_I2C)
-	{
+	if (global_comms_type == VL53LX_I2C) {
 		// TODO
 
-		if(status != VL53LX_ERROR_NONE)
-		{
-			trace_i2c("VL53LX_ReadMulti: RANGING_SENSOR_COMMS_Read_CCI() failed\n");
+		if (status != VL53LX_ERROR_NONE) {
+			trace_i2c(
+				"VL53LX_ReadMulti: RANGING_SENSOR_COMMS_Read_CCI() failed\n");
 		}
-	}
-	else if(global_comms_type == VL53LX_SPI)
-	{
+	} else if (global_comms_type == VL53LX_SPI) {
 		// unused
-	}
-	else
-	{
-		trace_i2c("VL53LX_ReadMulti: Comms must be one of VL53LX_I2C or VL53LX_SPI\n");
+	} else {
+		trace_i2c(
+			"VL53LX_ReadMulti: Comms must be one of VL53LX_I2C or VL53LX_SPI\n");
 		status = VL53LX_ERROR_CONTROL_INTERFACE;
 	}
 
 	return status;
 }
 
-
-VL53LX_Error VL53LX_WrByte(
-	VL53LX_Dev_t *pdev,
-	uint16_t      index,
-	uint8_t       VL53LX_p_003)
+VL53LX_Error VL53LX_WrByte(VL53LX_Dev_t *pdev, uint16_t index,
+			   uint8_t data)
 {
-	VL53LX_Error status         = VL53LX_ERROR_NONE;
-	uint8_t  buffer[1];
+	VL53LX_Error status = VL53LX_ERROR_NONE;
 
-
-	buffer[0] = (uint8_t)(VL53LX_p_003);
-
-	status = VL53LX_WriteMulti(pdev, index, buffer, 1);
+	status = VL53LX_WriteMulti(pdev, index, &data, 1);
 
 	return status;
 }
 
-
-VL53LX_Error VL53LX_WrWord(
-	VL53LX_Dev_t *pdev,
-	uint16_t      index,
-	uint16_t      VL53LX_p_003)
+VL53LX_Error VL53LX_WrWord(VL53LX_Dev_t *pdev, uint16_t index,
+			   uint16_t VL53LX_p_003)
 {
-	VL53LX_Error status         = VL53LX_ERROR_NONE;
-	uint8_t  buffer[2];
-
+	VL53LX_Error status = VL53LX_ERROR_NONE;
+	uint8_t buffer[2];
 
 	buffer[0] = (uint8_t)(VL53LX_p_003 >> 8);
-	buffer[1] = (uint8_t)(VL53LX_p_003 &  0x00FF);
+	buffer[1] = (uint8_t)(VL53LX_p_003 & 0x00FF);
 
 	status = VL53LX_WriteMulti(pdev, index, buffer, VL53LX_BYTES_PER_WORD);
 
 	return status;
 }
 
-
-VL53LX_Error VL53LX_WrDWord(
-	VL53LX_Dev_t *pdev,
-	uint16_t      index,
-	uint32_t      VL53LX_p_003)
+VL53LX_Error VL53LX_WrDWord(VL53LX_Dev_t *pdev, uint16_t index,
+			    uint32_t VL53LX_p_003)
 {
-	VL53LX_Error status         = VL53LX_ERROR_NONE;
-	uint8_t  buffer[4];
+	VL53LX_Error status = VL53LX_ERROR_NONE;
+	uint8_t buffer[4];
 
-
-	buffer[0] = (uint8_t) (VL53LX_p_003 >> 24);
-	buffer[1] = (uint8_t)((VL53LX_p_003 &  0x00FF0000) >> 16);
-	buffer[2] = (uint8_t)((VL53LX_p_003 &  0x0000FF00) >> 8);
-	buffer[3] = (uint8_t) (VL53LX_p_003 &  0x000000FF);
+	buffer[0] = (uint8_t)(VL53LX_p_003 >> 24);
+	buffer[1] = (uint8_t)((VL53LX_p_003 & 0x00FF0000) >> 16);
+	buffer[2] = (uint8_t)((VL53LX_p_003 & 0x0000FF00) >> 8);
+	buffer[3] = (uint8_t)(VL53LX_p_003 & 0x000000FF);
 
 	status = VL53LX_WriteMulti(pdev, index, buffer, VL53LX_BYTES_PER_DWORD);
 
 	return status;
 }
 
-
-VL53LX_Error VL53LX_RdByte(
-	VL53LX_Dev_t *pdev,
-	uint16_t      index,
-	uint8_t      *pdata)
+VL53LX_Error VL53LX_RdByte(VL53LX_Dev_t *pdev, uint16_t index, uint8_t *pdata)
 {
-	VL53LX_Error status         = VL53LX_ERROR_NONE;
-	uint8_t  buffer[1];
+	VL53LX_Error status = VL53LX_ERROR_NONE;
+	uint8_t buffer[1] = {0};
 
 	status = VL53LX_ReadMulti(pdev, index, buffer, 1);
 
@@ -264,53 +167,34 @@ VL53LX_Error VL53LX_RdByte(
 	return status;
 }
 
-
-VL53LX_Error VL53LX_RdWord(
-	VL53LX_Dev_t *pdev,
-	uint16_t      index,
-	uint16_t     *pdata)
-{
-	VL53LX_Error status         = VL53LX_ERROR_NONE;
-	uint8_t  buffer[2];
-
-	status = VL53LX_ReadMulti(
-					pdev,
-					index,
-					buffer,
-					VL53LX_BYTES_PER_WORD);
-
-	*pdata = (uint16_t)(((uint16_t)(buffer[0])<<8) + (uint16_t)buffer[1]);
-
-	return status;
-}
-
-
-VL53LX_Error VL53LX_RdDWord(
-	VL53LX_Dev_t *pdev,
-	uint16_t      index,
-	uint32_t     *pdata)
+VL53LX_Error VL53LX_RdWord(VL53LX_Dev_t *pdev, uint16_t index, uint16_t *pdata)
 {
 	VL53LX_Error status = VL53LX_ERROR_NONE;
-	uint8_t  buffer[4];
+	uint8_t buffer[2] = {0};
 
-	status = VL53LX_ReadMulti(
-					pdev,
-					index,
-					buffer,
-					VL53LX_BYTES_PER_DWORD);
+	status = VL53LX_ReadMulti(pdev, index, buffer, VL53LX_BYTES_PER_WORD);
 
-	*pdata = ((uint32_t)buffer[0]<<24) + ((uint32_t)buffer[1]<<16) + ((uint32_t)buffer[2]<<8) + (uint32_t)buffer[3];
+	*pdata = (uint16_t)(((uint16_t)(buffer[0]) << 8) + (uint16_t)buffer[1]);
 
 	return status;
 }
 
-
-
-VL53LX_Error VL53LX_WaitUs(
-	VL53LX_Dev_t *pdev,
-	int32_t       wait_us)
+VL53LX_Error VL53LX_RdDWord(VL53LX_Dev_t *pdev, uint16_t index, uint32_t *pdata)
 {
-	VL53LX_Error status         = VL53LX_ERROR_NONE;
+	VL53LX_Error status = VL53LX_ERROR_NONE;
+	uint8_t buffer[4] = {0};
+
+	status = VL53LX_ReadMulti(pdev, index, buffer, VL53LX_BYTES_PER_DWORD);
+
+	*pdata = ((uint32_t)buffer[0] << 24) + ((uint32_t)buffer[1] << 16) +
+		 ((uint32_t)buffer[2] << 8) + (uint32_t)buffer[3];
+
+	return status;
+}
+
+VL53LX_Error VL53LX_WaitUs(VL53LX_Dev_t *pdev, int32_t wait_us)
+{
+	VL53LX_Error status = VL53LX_ERROR_NONE;
 
 	// TODO wait_us
 
@@ -319,189 +203,34 @@ VL53LX_Error VL53LX_WaitUs(
 	return status;
 }
 
-
-VL53LX_Error VL53LX_WaitMs(
-	VL53LX_Dev_t *pdev,
-	int32_t       wait_ms)
+VL53LX_Error VL53LX_WaitMs(VL53LX_Dev_t *pdev, int32_t wait_ms)
 {
 	return VL53LX_WaitUs(pdev, wait_ms * 1000);
 }
 
-
-
-VL53LX_Error VL53LX_GetTimerFrequency(int32_t *ptimer_freq_hz)
+VL53LX_Error VL53LX_GetTickCount(uint32_t *ptick_count_ms)
 {
-	*ptimer_freq_hz = 0;
-
-	trace_print(VL53LX_TRACE_LEVEL_INFO, "VL53LX_GetTimerFrequency: Freq : %dHz\n", *ptimer_freq_hz);
-	return VL53LX_ERROR_NONE;
-}
-
-
-VL53LX_Error VL53LX_GetTimerValue(int32_t *ptimer_count)
-{
-	*ptimer_count = 0;
-
-	trace_print(VL53LX_TRACE_LEVEL_INFO, "VL53LX_GetTimerValue: Freq : %dHz\n", *ptimer_count);
-	return VL53LX_ERROR_NONE;
-}
-
-
-
-
-VL53LX_Error VL53LX_GpioSetMode(uint8_t pin, uint8_t mode)
-{
-	VL53LX_Error status         = VL53LX_ERROR_NONE;
-
-	// TODO
-
-	trace_print(VL53LX_TRACE_LEVEL_INFO, "VL53LX_GpioSetMode: Status %d. Pin %d, Mode %d\n", status, pin, mode);
-	return status;
-}
-
-
-VL53LX_Error  VL53LX_GpioSetValue(uint8_t pin, uint8_t value)
-{
-	VL53LX_Error status         = VL53LX_ERROR_NONE;
-
-	// TODO
-
-	trace_print(VL53LX_TRACE_LEVEL_INFO, "VL53LX_GpioSetValue: Status %d. Pin %d, Mode %d\n", status, pin, value);
-	return status;
-
-}
-
-
-VL53LX_Error  VL53LX_GpioGetValue(uint8_t pin, uint8_t *pvalue)
-{
-	VL53LX_Error status         = VL53LX_ERROR_NONE;
-
-	// TODO
-
-	trace_print(VL53LX_TRACE_LEVEL_INFO, "VL53LX_GpioGetValue: Status %d. Pin %d, Mode %d\n", status, pin, *pvalue);
-	return status;
-}
-
-
-
-VL53LX_Error  VL53LX_GpioXshutdown(uint8_t value)
-{
-	VL53LX_Error status         = VL53LX_ERROR_NONE;
-
-	if(status == VL53LX_ERROR_NONE)
-	{
-		// TODO set mode
-		//status = VL53LX_GpioSetMode((uint8_t)GPIO_XSHUTDOWN, (uint8_t)GPIO_OutputPP);
-	}
-
-	if(status == VL53LX_ERROR_NONE)
-	{
-		if(value)
-		{
-			// TODO set high
-		}
-		else
-		{
-			// TODO set low
-		}
-	}
-
-	trace_print(VL53LX_TRACE_LEVEL_INFO, "VL53LX_GpioXShutdown: Status %d. Value %d\n", status, value);
-	return status;
-}
-
-
-VL53LX_Error  VL53LX_GpioCommsSelect(uint8_t value)
-{
-	VL53LX_Error status         = VL53LX_ERROR_NONE;
-
-	if(status == VL53LX_ERROR_NONE)
-	{
-		// unused (set GPIO mode)
-	}
-
-	if(status == VL53LX_ERROR_NONE)
-	{
-		if(value)
-		{
-			// unused (SPI chip select)
-		}
-		else
-		{
-			// unused
-		}
-	}
-
-	trace_print(VL53LX_TRACE_LEVEL_INFO, "VL53LX_GpioCommsSelect: Status %d. Value %d\n", status, value);
-	return status;
-}
-
-
-VL53LX_Error  VL53LX_GpioPowerEnable(uint8_t value)
-{
-	VL53LX_Error status         = VL53LX_ERROR_NONE;
-	(void)value;
-	// Unused
-	return status;
-}
-
-
-VL53LX_Error  VL53LX_GpioInterruptEnable(void (*function)(void), uint8_t edge_type)
-{
-	VL53LX_Error status         = VL53LX_ERROR_NONE;
-
-	SUPPRESS_UNUSED_WARNING(function);
-	SUPPRESS_UNUSED_WARNING(edge_type);
-
-	return status;
-}
-
-
-VL53LX_Error  VL53LX_GpioInterruptDisable(void)
-{
-	VL53LX_Error status         = VL53LX_ERROR_NONE;
-
-	return status;
-}
-
-
-VL53LX_Error VL53LX_GetTickCount(
-	uint32_t *ptick_count_ms)
-{
-
-
-
-	VL53LX_Error status  = VL53LX_ERROR_NONE;
+	VL53LX_Error status = VL53LX_ERROR_NONE;
 
 	// TODO get tick
 
-	trace_print(
-	VL53LX_TRACE_LEVEL_DEBUG,
-	"VL53LX_GetTickCount() = %5u ms;\n",
-	*ptick_count_ms);
+	trace_print(VL53LX_TRACE_LEVEL_DEBUG,
+		    "VL53LX_GetTickCount() = %5u ms;\n", *ptick_count_ms);
 
 	return status;
-
 }
 
-
-VL53LX_Error VL53LX_WaitValueMaskEx(
-	VL53LX_Dev_t *pdev,
-	uint32_t      timeout_ms,
-	uint16_t      index,
-	uint8_t       value,
-	uint8_t       mask,
-	uint32_t      poll_delay_ms)
+VL53LX_Error VL53LX_WaitValueMaskEx(VL53LX_Dev_t *pdev, uint32_t timeout_ms,
+				    uint16_t index, uint8_t value, uint8_t mask,
+				    uint32_t poll_delay_ms)
 {
-
-
-	VL53LX_Error status         = VL53LX_ERROR_NONE;
-	uint32_t     start_time_ms   = 0;
-	uint32_t     current_time_ms = 0;
-	uint8_t      byte_value      = 0;
-	uint8_t      found           = 0;
+	VL53LX_Error status = VL53LX_ERROR_NONE;
+	uint32_t start_time_ms = 0;
+	uint32_t current_time_ms = 0;
+	uint8_t byte_value = 0;
+	uint8_t found = 0;
 #ifdef VL53LX_LOG_ENABLE
-	uint32_t     trace_functions = 0;
+	uint32_t trace_functions = 0;
 #endif
 
 	_LOG_STRING_BUFFER(register_name);
@@ -510,50 +239,33 @@ VL53LX_Error VL53LX_WaitValueMaskEx(
 
 #ifdef VL53LX_LOG_ENABLE
 
-	VL53LX_get_register_name(
-			index,
-			register_name);
-
+	VL53LX_get_register_name(index, register_name);
 
 	trace_i2c("WaitValueMaskEx(%5d, %s, 0x%02X, 0x%02X, %5d);\n",
-		timeout_ms, register_name, value, mask, poll_delay_ms);
+		  timeout_ms, register_name, value, mask, poll_delay_ms);
 #endif
-
-
 
 	VL53LX_GetTickCount(&start_time_ms);
 	pdev->new_data_ready_poll_duration_ms = 0;
-
-
 
 #ifdef VL53LX_LOG_ENABLE
 	trace_functions = _LOG_GET_TRACE_FUNCTIONS();
 #endif
 	_LOG_SET_TRACE_FUNCTIONS(VL53LX_TRACE_FUNCTION_NONE);
 
-
-
 	while ((status == VL53LX_ERROR_NONE) &&
-		   (pdev->new_data_ready_poll_duration_ms < timeout_ms) &&
-		   (found == 0))
-	{
-		status = VL53LX_RdByte(
-						pdev,
-						index,
-						&byte_value);
+	       (pdev->new_data_ready_poll_duration_ms < timeout_ms) &&
+	       (found == 0)) {
+		status = VL53LX_RdByte(pdev, index, &byte_value);
 
-		if ((byte_value & mask) == value)
-		{
+		if ((byte_value & mask) == value) {
 			found = 1;
 		}
 
-
-
-
 		VL53LX_GetTickCount(&current_time_ms);
-		pdev->new_data_ready_poll_duration_ms = current_time_ms - start_time_ms;
+		pdev->new_data_ready_poll_duration_ms =
+			current_time_ms - start_time_ms;
 	}
-
 
 	_LOG_SET_TRACE_FUNCTIONS(trace_functions);
 
@@ -562,5 +274,3 @@ VL53LX_Error VL53LX_WaitValueMaskEx(
 
 	return status;
 }
-
-
--- src/vl53l3cx/vl53lx_platform_user_defines.h
+++ src/vl53l3cx/vl53lx_platform_user_defines.h
@@ -33,49 +33,25 @@
 
 */
 
-
 #ifndef _VL53LX_PLATFORM_USER_DEFINES_H_
 #define _VL53LX_PLATFORM_USER_DEFINES_H_
 
 #ifdef __cplusplus
-extern "C"
-{
+extern "C" {
 #endif
-
-
-
-
 
 #define do_division_u(dividend, divisor) (dividend / divisor)
 
-
-
 #define do_division_s(dividend, divisor) (dividend / divisor)
 
+#define WARN_OVERRIDE_STATUS(__X__)                                            \
+	trace_print(VL53LX_TRACE_LEVEL_WARNING, #__X__);
 
-
-#define WARN_OVERRIDE_STATUS(__X__)\
-	trace_print (VL53LX_TRACE_LEVEL_WARNING, #__X__);
-
-
-#ifdef _MSC_VER
-#define DISABLE_WARNINGS() { \
-	__pragma (warning (push)); \
-	__pragma (warning (disable:4127)); \
-	}
-#define ENABLE_WARNINGS() { \
-	__pragma (warning (pop)); \
-	}
-#else
-	#define DISABLE_WARNINGS()
-	#define ENABLE_WARNINGS()
-#endif
-
+#define DISABLE_WARNINGS()
+#define ENABLE_WARNINGS()
 
 #ifdef __cplusplus
 }
 #endif
 
 #endif
-
-
